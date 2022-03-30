b8befe20adbcf0280cc729bd646020b3bef57c83
karinadesyatnik <karinadesyatnik27@gmail.com>
2022-03-30 12:33:40 +0300

    home page

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,17 @@ $(function(){
 	})
 
 
+
+ $(".top").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+
+    });
+  
+
+
 // ------start sticky header------
 
 $(window).scroll(function() {
